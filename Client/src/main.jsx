9de4cd973a7d487de1d65565ded3c69564dc5c59
Nardos-26 <nardosteshome95@gmail.com>
2025-07-01 13:49:50 -0400

    fix

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./commonResource/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./commonResource/css/styles.css";
-import "./commonResource/js/bootstrap.js";
-import "./commonResource/js/custom.js";
+import "bootstrap";
+// import "./commonResource/css/bootstrap.css";
+// import "./commonResource/css/styles.css";
+// import "./commonResource/js/bootstrap.js";
+// import "./commonResource/js/custom.js";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 createRoot(document.getElementById("root")).render(
